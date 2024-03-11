59417659f7d30b8dcf69ac3568321938a2a20ce5
NathanMB77 <160797692+NathanMB77@users.noreply.github.com>
2024-03-11 13:46:02 +0000

    First commit

--- src/app.js
+++ src/app.js
@@ -7,5 +7,24 @@ import "./assets/img/4geeks.ico";
 
 window.onload = function() {
   //write your code here
-  console.log("Hello Rigo from the console!");
+  let who = ["The dog", "My grandma", "His turtle", "My bird", "Santa Claus"];
+  let action = ["ate", "peed", "crushed", "broke", "shat on"];
+  let what = ["my homework", "the keys", "the car", "the bed"];
+  let when = [
+    "before the class",
+    "right on time",
+    "when I finished",
+    "during my lunch",
+    "while I was praying",
+    "while I was on the toilet"
+  ];
+
+  let word1 = who[Math.floor(Math.random() * 5)];
+  let word2 = action[Math.floor(Math.random() * 5)];
+  let word3 = what[Math.floor(Math.random() * 4)];
+  let word4 = when[Math.floor(Math.random() * 6)];
+
+  let sentence = word1 + " " + word2 + " " + word3 + " " + word4 + ".";
+
+  document.getElementById("excuse").innerHTML = sentence;
 };
